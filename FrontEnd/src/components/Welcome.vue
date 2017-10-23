427a78f75cf4f0e63afad9e3b8e8362620f61a27
Marchandise Rudy <r.marchandise@sfr.fr>
2017-10-23 16:15:59 +0200

    Fixup database

--- FrontEnd/src/components/Welcome.vue
+++ FrontEnd/src/components/Welcome.vue
@@ -13,14 +13,30 @@
               </v-grid>
             </v-row>
 
-            <h4 v-if="ean != ''">Informations</h4>
+            <v-row v-if="ean != ''">
+              <h4>Informations</h4>
 
-            <ul v-if="ean != ''">
-              <li><b>Position</b>: {{ position.lat }};{{ position.lng }}</li>
-              <li><b>Distance</b>: {{ Math.round(distance()) }}M / {{ Math.floor(distance() * 3,28084) }}Ft</li>
-              <li><b>Speed</b>: {{ Math.round(speed) }} Km/h | {{ Math.round(speed * 0.6213711923) }} Mi/h</li>
-              <li><b>Date</b>: {{ getMoment().format('MMMM Do YYYY, hh:mm:ss A') }} / {{ getMoment().fromNow() }}</li>
-            </ul>
+              <ul>
+                <li><b>Position</b>: {{ position.lat }};{{ position.lng }}</li>
+                <li><b>Distance</b>: {{ Math.round(distance()) }}M / {{ Math.floor(distance() * 3,28084) }}Ft</li>
+                <li><b>Speed</b>: <v-chip>{{ Math.round(speed) }} Km/h</v-chip> | <v-chip>{{ Math.round(speed * 0.6213711923) }} Mi/h</v-chip></li>
+                <li><b>Date</b>: {{ getMoment().format('MMMM Do YYYY, hh:mm:ss A') }} / {{ getMoment().fromNow() }}</li>
+              </ul>
+
+              <v-row v-if="history.length > 0">
+                <h4>History</h4>
+
+                <v-collection class="history">
+                  <v-collection-avatar v-for="H in history" v-bind:key="H.pos">
+                    <span class="title">{{H.pos}}</span>
+                    <p>{{H.speed}} <br>
+                      {{H.date}}
+                    </p>
+                    <span slot="secondary"><v-icon>map</v-icon></span>
+                  </v-collection-avatar>
+                </v-collection>
+              </v-row>
+            </v-row>
       </v-grid>
 
       <v-grid m7 v-if="ean != ''">
@@ -57,7 +73,8 @@ export default {
       lastPosition: {lat: 0.0, lng: 0.0},
       speed: 0,
       date: null,
-      lastDate: null
+      lastDate: null,
+      history: []
     }
   },
   methods: {
@@ -123,6 +140,12 @@ export default {
           lat: parseFloat(val.lat),
           lng: parseFloat(val.lon)
         }
+        // ----- HISTORY
+        this.history.push({
+          pos: this.position.lat + ';' + this.lastPosition.lng,
+          speed: Math.round(this.distance()) + 'M / ' + Math.floor(this.distance() * 3.28084) + 'Ft',
+          date: this.getMoment().format('MMMM Do YYYY, hh:mm:ss A') + '/' + this.getMoment().fromNow()
+        })
         // Inverse
         this.lastPosition = this.position
         this.lastDate = this.date
@@ -140,5 +163,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.history {
+  height: 350px;
+  overflow-y: scroll;
+}
 </style>
